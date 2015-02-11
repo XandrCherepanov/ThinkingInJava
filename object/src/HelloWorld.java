b0063c384fa284ef2e8d40ac8e697e760d2a73a7
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2015-02-11 20:39:25 +0500

    Objects chapter: Exercise 2

---
+++ object/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Exercise 2: (1) Following the HelloDate.java example in this chapter,
+ * create a “hello, world” program that simply displays that statement.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
